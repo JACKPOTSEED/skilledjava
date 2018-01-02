be61e0ce6b8146428943daaccbc1852f3326e3ba
jackpotseed <riris8756@naver.com>
2018-01-03 01:30:52 +0900

    18.1.3 커밋

--- src/kr/sangj/polymorphism/PolymorphismExam.java
+++ src/kr/sangj/polymorphism/PolymorphismExam.java
@@ -8,14 +8,18 @@ import java.util.Map;
 public class PolymorphismExam {
 
 	public void print() {
-	
-		
+			
 		List<HashMap<String, Object>> priceList = new ArrayList<HashMap<String,Object>>();
 		//이게 동작 할거라고 생각했는데 동작하지 않고 위에 것이 동작함.
 		/*List<Map> listMap = new ArrayList<HashMap<String, Object>>();*/ 
 		
 		/*Map resMap = new HashMap<String,Object>();*/
 		Map resMap = new HashMap<String,Object>();
+		
+		HashMap<String, Object> hasMap = new HashMap<>();
+		
+		/*hasMap.*/
+		/*resMap.*/
 		resMap.put("result", true);
 		
 		System.out.println("resMap : " + resMap);
